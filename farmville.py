abd844fed73ec02978b0b8324a28f22f706265e1
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2009-12-21 19:14:57 +0200

    farmville: add mouse position calculation

--- farmville.py
+++ farmville.py
@@ -23,11 +23,26 @@ def parse_options():
                       help='set logging level to DEBUG')
     return parser.parse_args()
 
+def do_action(x, y):
+    logging.info('do_action(%d, %d)' % (x, y))
+
 def main():
     options, args = parse_options()
     nrows, ncols = (int(x) for x in args)
     logging.basicConfig(level=options.log_level)
     logging.debug('zoom = %d, ncols = %d, nrows = %d' % (options.zoom, ncols, nrows))
+
+    x, y = mousecontrol.get_mouse_position()
+    logging.info('initial mouse position: (%d, %d)' % (x, y))
+    xdelta = options.zoom * XDIST
+    ydelta = options.zoom * YDIST
+    for i in range(nrows):
+        for j in range(ncols):
+            do_action(x, y)
+            x += xdelta
+        x -= xdelta # cancel last move
+        xdelta = -xdelta # reverse direction
+        y += ydelta
 
 if __name__ == '__main__':
     main()
