5d0fd72249d5dbbd664a18137647e7c7a28dd490
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2009-12-21 19:39:18 +0200

    cosmetic changes

--- farmville.py
+++ farmville.py
@@ -53,19 +53,19 @@ def parse_options():
     parser.set_defaults(dry_run=False, zoom=1, delay=0.5, log_level=logging.WARN)
     parser.add_option('-n', '--dry-run',
                       action='store_true', dest='dry_run',
-                      help="""Don't actually run any commands; just print them.""")
+                      help="""don't actually run any commands; just print them.""")
     parser.add_option('-z', '--zoom',
                       type='int', dest='zoom',
-                      help='zoom level: 1 - 4')
+                      help="zoom level: 1 - 4")
     parser.add_option('--delay',
                       type='float', dest='delay',
-                      help='delay between mouse actions')
+                      help="delay between mouse actions")
     parser.add_option('-v', '--verbose',
                       action='store_const', const=logging.INFO, dest='log_level',
-                      help='set logging level to INFO')
+                      help="set logging level to INFO")
     parser.add_option('--debug',
                       action='store_const', const=logging.DEBUG, dest='log_level',
-                      help='set logging level to DEBUG')
+                      help="set logging level to DEBUG")
     return parser.parse_args()
 
 def main():
