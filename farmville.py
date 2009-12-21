53e75e75bb4cc7f53500a4a042df178cc2792fd1
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2009-12-21 18:42:03 +0200

    farmville: add CLI option parser & logging

--- farmville.py
+++ farmville.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
+import logging
+from optparse import OptionParser
 import sys
 import mousecontrol
 
 def main():
-    pass
+    parser = OptionParser()
+    parser.set_defaults(zoom=1, log_level=logging.WARN)
+    parser.add_option('-z', '--zoom',
+                      type='int', dest='zoom',
+                      help='zoom level: 1 - 4')
+    parser.add_option('-v', '--verbose',
+                      action='store_const', const=logging.INFO, dest='log_level',
+                      help='set logging level to INFO')
+    parser.add_option('--debug',
+                      action='store_const', const=logging.DEBUG, dest='log_level',
+                      help='set logging level to DEBUG')
+    options, args = parser.parse_args()
+    logging.basicConfig(level=options.log_level)
+
+    nrows, ncols = (int(x) for x in args)
+    logging.debug('zoom = %d, ncols = %d, nrows = %d' % (options.zoom, ncols, nrows))
 
 if __name__ == '__main__':
     main()
