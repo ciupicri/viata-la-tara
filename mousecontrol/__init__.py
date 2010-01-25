729b8a9a6132db444cf0d65ade2f4b2c9c618370
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2010-01-25 08:43:31 +0200

    mousecontrol: add X11 backend

--- mousecontrol/__init__.py
+++ mousecontrol/__init__.py
@@ -1,34 +1,4 @@
-#
-# Based on code from http://ubuntuforums.org/showpost.php?p=4862593&postcount=7
-#
-import Xlib.display
-import Xlib.ext.xtest
+import os
 
-_display = Xlib.display.Display()
-_screen = _display.screen()
-_root = _screen.root
-
-def mouse_click(button=1):
-    """button = 1 left, 2 middle, 3 right"""
-    mouse_down(button)
-    mouse_up(button)
-
-def mouse_down(button): 
-    Xlib.ext.xtest.fake_input(_display, Xlib.X.ButtonPress, button)
-    _display.sync()
-
-def mouse_up(button):
-    Xlib.ext.xtest.fake_input(_display, Xlib.X.ButtonRelease, button)
-    _display.sync()
-
-def mouse_warp(x, y):
-    _root.warp_pointer(x, y)
-    _display.sync()
-
-def get_mouse_position():
-    """get_mouse_position() -> x, y"""
-    d = _root.query_pointer()._data
-    return d['root_x'], d['root_y']
-
-def get_screen_resolution():
-    return _screen['width_in_pixels'], _screen['height_in_pixels']
+if 'DISPLAY' in os.environ:
+    from .x11 import *
